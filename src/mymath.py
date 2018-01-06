08ec43434937361f677559e2556f046ab9f715f7
furuhama <furuhama@users.noreply.github.com>
2018-01-06 17:39:19 +0900

    Add some basic functions

--- src/mymath.py
+++ src/mymath.py
@@ -3,8 +3,29 @@ mymath is my functions
 """
 
 
-def add(a, b):
+def add(x, y):
     """
     add returns sum of its two arguments
     """
-    return a + b
+    return x + y
+
+
+def mul(x, y):
+    """
+    mul returns multiply of two arguments
+    """
+    return x * y
+
+
+def square(x):
+    """
+    square returns square of a argument
+    """
+    return mul(x, x)
+
+
+def sum_squares(x, y):
+    """
+    sum_squares returns sum of square of two arguments
+    """
+    return add(square(x), square(y))
