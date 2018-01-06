0c9f2ac5d3fff68ec6e70f9a99576d97d50af865
furuhama <furuhama@users.noreply.github.com>
2018-01-06 17:30:18 +0900

    Add main.py & mymath.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+main script
+"""
+
+from src import mymath
+
+
+def main():
+    """
+    main function
+    """
+    result = mymath.add(1, 2)
+    print(result)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/mymath.py
@@ -0,0 +1,10 @@
+"""
+mymath is my functions
+"""
+
+
+def add(a, b):
+    """
+    add returns sum of its two arguments
+    """
+    return a + b
