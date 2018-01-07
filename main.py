4490a88e5771e43b5ace621ffaca4ead81e32b5c
furuhama <furuhama@users.noreply.github.com>
2018-01-07 10:57:40 +0900

    Edit main.py

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ main script
 """
 
 # from src import mymath
-from src import myabstract
+# from src import myabstract
+from src import myimprove
 
 
 def main():
     """
     main function
     """
-    result = myabstract.pi_sum(1000000)
+    result = myimprove.iter_improve(
+        myimprove.golden_update, myimprove.golden_test)
     print(result)
     return 0
 
