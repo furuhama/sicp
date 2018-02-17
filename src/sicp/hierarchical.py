e6b4c18ce17268bda3128381890d6b9d997fe971
furuhama <furuhama@users.noreply.github.com>
2018-02-17 10:29:25 +0900

    Add fib_tree

--- src/sicp/hierarchical.py
+++ src/sicp/hierarchical.py
@@ -7,3 +7,29 @@ def count_leaves(tree):
     if not isinstance(tree, tuple) :
         return 1
     return sum(map(count_leaves, tree))
+
+def map_tree(tree, fn):
+    if not isinstance(tree, tuple):
+        return fn(tree)
+    return tuple(map_tree(branch, fn) for branch in tree)
+
+class Tree(object):
+    def __init__(self, entry, left=None, right=None):
+        self.entry = entry
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        args = repr(self.entry)
+        if self.left or self.right:
+            args += ', {0}, {1}'.format(repr(self.left), repr(self.right))
+        return 'Tree({0})'.format(args)
+
+def fib_tree(n):
+    if n == 1:
+        return Tree(0)
+    if n == 2:
+        return Tree(1)
+    left = fib_tree(n-2)
+    right = fib_tree(n-1)
+    return Tree(left.entry + right.entry, left, right)
