244012844d8eda1f4105d5247ea34740f0a207aa
furuhama <furuhama@users.noreply.github.com>
2018-02-17 22:17:52 +0900

    Add list & equivalence

--- gauche/hello.scm
+++ gauche/hello.scm
@@ -43,7 +43,7 @@
 (let ((x 5) (y (+ x 1))) (print y)) ; 4
 (let* ((x 5) (y (+ x 1))) (print y)) ; 6
 
-; local recursive definition
+;; local recursive definition
 (letrec ((even?
            (lambda (n)
              (if (zero? n)
@@ -57,7 +57,7 @@
   (print (even? 3872))
   (print (odd? 3265)))
 
-; global definition
+;; global definition
 (define (hard-even? x)
   (define (even? n)
     (if (zero? n)
@@ -71,17 +71,66 @@
 
 (print (hard-even? 10))
 
-; arithmetic calcilation
-
+;; arithmetic calcilation
 (print (- (quotient (* (+ 3 7 10) (- 1000 8)) 992) 17))
 
 (print (remainder 27 4))
 
 (print (not (= 15 14)))
 
-; list
+;; list
 (print (cons 'a 'b)) ; (a . b)
 
 (print (list 'a 'b )) ; (a b)
 
 (print (cons 'a (cons 'b '()))) ; (a b)
+
+(print (car (cons 'a 'b))) ; a
+
+(print (cdr (cons 'a 'b))) ; b
+
+(print (cdr (list 'a 'b))) ; (b)
+
+(print (cadr '(a b))) ; (car (cdr '(a b))) ; b
+
+(print (list-tail '(a b c) 0)) ; (a b c)
+
+(print (list-tail '(a b c) 1)) ; (b c)
+
+(print (list-tail '(a b c) 2)) ; (c)
+
+(print (list-tail '(a b c) 3)) ; ()
+
+; (print (list-tail '(a b c) 4)) ; error
+
+(print (list-ref '(a b c) 0)) ; a
+
+(print (append '(a b) '(c d) '() '(e))) ; (a b c d e)
+
+(define L1 (list 'a 'b 'c))
+(define L2 (list 'd))
+(define L3 (append L1 L2)) ; L1 part is fixed in this line
+(set-car! L1 1)
+(print L1) ; (1 b c)
+(set-car! L2 2)
+(print L2) ; (2)
+(print L3) ; (a b c 2) ; only L2 part is changed
+
+(print (null? '())) ; #t
+
+(print (list? '(a b))) ; #t
+
+(print (list? '(a . b))) ; #f
+
+;; equivalence
+(print (eqv? 'a 'a)) ; #t
+
+(print (eqv? 'a 'b)) ; #f
+
+(print (eqv? 100 (+ 50 50))) ; #t
+
+(print (eqv? (list 'a 'b) (list 'a 'b))) ; #f
+
+(print (equal? (list 'a 'b) (list 'a 'b))) ; #t
+
+;; types
