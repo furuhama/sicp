1033f6086f45c7d8218bb6f5838d8240ecb098ab
furuhama <furuhama@users.noreply.github.com>
2018-02-17 21:24:40 +0900

    Add cond, case, let

--- gauche/hello.scm
+++ gauche/hello.scm
@@ -4,7 +4,36 @@
 
 (print '(+ 1 2)) ; (+ 1 2)
 
+;; global definition
 (define x 3)
 (print x) ; 3
 
 (print (quote x)) ; x
+
+(print (quote 'x)) ; 'x
+
+(print (and (= 2 2) (> 2 1))) ; #t
+
+(print (or (> 2 2) '(a b))) ; (a b)
+
+;; condition control statement
+(print (cond ((> 3 2) 'greater)
+             ((< 3 2) 'less))) ; greater
+
+;; case statement
+(print (case (* 2 3)
+         ((2 3 5 7) 'prime)
+         ((1 4 6 8 9) 'composite))) ; composite
+
+(print (case (car '(c d))
+         ((a e i o u) 'vowel)
+         ((w y) 'semivowel)
+         (else 'consonant))) ; consonant
+
+;; local definition
+(let ((x 2) (y 3))
+  (print (* x y))) ; 6
+
+(let ((x 2) (y 3)) ; x = 2, y = 3
+  (let ((x 7) (z (+ x y))) ; x = 7, y = 3, z = 5
+    (print (* z x)))) ;; 35
