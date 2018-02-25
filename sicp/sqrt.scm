f6f374861e79d00b8678b25ad6e835367ae2e4a5
furuhama <furuhama@users.noreply.github.com>
2018-02-26 00:46:36 +0900

    Add sqrt.scm

---
+++ sicp/sqrt.scm
@@ -0,0 +1,19 @@
+;; define sqrt function
+(define (sqrt x)
+  (define (sqrt-iter guess x)
+    (define (good-enough? guess x)
+      (define (square x)
+        (* x x))
+      (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess x)
+      (define (average x y)
+        (/ (+ x y) 2))
+      (average guess (/ x guess)))
+    (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+
+;; main process
+
+(print (sqrt 9))
