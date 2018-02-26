7d7387077e3fe348b8e59871aaf420c06d246079
furuhama <furuhama@users.noreply.github.com>
2018-02-26 10:14:28 +0900

    Add fib

---
+++ sicp/src/chap1/fib.scm
@@ -0,0 +1,11 @@
+;; calc fibonacci
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+;; main process
+
+(print (fib 10))
