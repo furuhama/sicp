93f01203180ea5933deb0ed7365400d2608ec1f7
furuhama <furuhama@users.noreply.github.com>
2018-02-26 09:30:25 +0900

    Add tail recursion factorial

--- sicp/src/factorial.scm
+++ sicp/src/factorial.scm
@@ -1,10 +1,23 @@
-;; factorial
+;; basic factorial
 
 (define (factorial n)
   (if (= n 1)
     1
     (* n (factorial (- n 1)))))
 
+;; tail recursion
+
+(define (t-fact n)
+  (define (fact-iter prod count max-count)
+    (if (> count max-count)
+      prod
+      (fact-iter (* count prod)
+                 (+ count 1)
+                 max-count)))
+  (fact-iter 1 1 n))
+
 ;; main process
 
 (print (factorial 10))
+
+(print (t-fact 10))
