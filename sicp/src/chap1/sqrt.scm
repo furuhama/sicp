fbc1b0d8594a95aceb75b51e75b2b1bc238b27ca
furuhama <furuhama@users.noreply.github.com>
2018-02-26 09:48:41 +0900

    Improved sqrt as square-root

--- sicp/src/chap1/sqrt.scm
+++ sicp/src/chap1/sqrt.scm
@@ -27,6 +27,31 @@
       (sqrt-iter (improve guess x) x)))
   (sqrt-iter 1.0 x))
 
+;; refactor improvement process
+;; evaluate guess number by ratio
+
+(define (square-root x)
+  (define (sqrt-iter guess x)
+    (define (good-enough? guess x)
+      (< (/ (abs (- ((lambda (x) (* x x)) guess) x)) x) 0.001))
+
+    (define (improve guess x)
+      ((lambda (x y) (/ (+ x y) 2)) guess (/ x guess)))
+
+    (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+
 ;; main process
 
 (print (sqrt 9))
+(print (square-root 9))
+
+(print (sqrt 90000001))
+(print (square-root 90000001))
+
+;; in this pattern, results are really different
+;; (square-root is the better way)
+(print (sqrt 0.00001))
+(print (square-root 0.00001))
